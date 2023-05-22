e0bf4a8b823810d51e7d266e78291a464d2c5214
Stefan Wetzig <stefanwetzig@yahoo.de>
2023-05-22 19:54:33 +0200

    add some sections, fix gallery

--- scripts/layout.js
+++
@@ -1,7 +0,0 @@
-function replaceNames() {
-    var portraits = document.getElementsByClassName('portrait');
-
-    for (i = 0; i < portraits.length; i++) {
-        portraits[i].style.backgroundImage = 'url(pics/ebenso/' + portraits[i].firstElementChild.innerText.toLowerCase() + '.png)';
-    }
-}
