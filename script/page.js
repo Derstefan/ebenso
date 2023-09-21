6195fe59b6e8c0d8462242d5d6d0d5fc0e6c8026
Bjoern Schmidt <bjoschmi84+git@gmail.com>
2023-09-21 18:43:35 +0200

    Bubble-Popups angepasst mit dunkler Farbe, hellem Text, Größe noch variabel

--- script/page.js
+++ script/page.js
@@ -1,13 +1,22 @@
 function showModal(element) {
     var modalText;
+    var modalHeading;
     var modalWindow = document.getElementById("modal");
     var modalContent = document.getElementById("modal-content");
+    var modalHeader = document.getElementById("modal-header");
     for (var i = 0; i < element.childNodes.length; i++) {
         if (element.childNodes[i].className == "bubble-modal") {
             modalText = element.childNodes[i].innerHTML;
             break;
         }
     }
+    for (var i = 0; i < element.childNodes.length; i++) {
+        if (element.childNodes[i].className == "bubble-name") {
+            modalHeading = element.childNodes[i].innerText;
+            break;
+        }
+    }
+    modalHeader.innerHTML = modalHeading;
     modalContent.innerHTML = modalText;
     modalWindow.style.display = "block";
 }
